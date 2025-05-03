23e68cc29741c34b0539c710fdd8f2a8ac10090d
jafeth gamboa <jafethgamboabaas@gmail.com>
2025-05-03 03:47:43 -0600

    Agregar estilos CSS para la página de usuarios y mejorar la estructura del contenido principal

--- admin/usuarios.php
+++ admin/usuarios.php
@@ -11,7 +11,7 @@ require_once 'elements/validationSession.php';
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/dashboard.css">
+    <link rel="stylesheet" href="css/usuarios.css">
     <script src="js/inactivity.js" defer></script>
 </head>
 
@@ -25,7 +25,15 @@ require_once 'elements/validationSession.php';
             include 'elements/sidebar.php';
             ?>
 
-
+            <!-- Main Content -->
+            <main class="col-md-9 col-lg-10 py-1 border main-content">
+                <h2 class="mb-4 tittle">USUARIOS</h2>
+                <hr class="custom-divider mb-4">
+                <div class="d-flex justify-content-center gap-3">
+                    <a href="ver_usuarios.php" class="btn btn-dark">VER USUARIOS</a>
+                    <a href="agregar_usuarios.php" class="btn btn-primary custom-btn">AGREGAR USUARIOS</a>
+                </div>
+            </main>
         </div>
     </div>
 </body>
