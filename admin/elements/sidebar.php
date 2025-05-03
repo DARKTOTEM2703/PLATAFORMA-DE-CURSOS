697195171282ad958f1bc712db6a44f122492179
jafeth gamboa <jafethgamboabaas@gmail.com>
2025-05-03 03:25:40 -0600

    Actualizar enlaces en la barra lateral y agregar archivos de usuarios y cursos con estructura básica

--- admin/elements/sidebar.php
+++ admin/elements/sidebar.php
@@ -2,13 +2,13 @@
 <nav class="col-md-3 col-lg-2 bg-primary sidebar py-4">
     <ul class="nav flex-column text-white">
         <li class="nav-item">
-            <a class="nav-link text-white" href="#">>> Inicio</a>
+            <a class="nav-link text-white" href="../admin/dashboard.php">>> Inicio</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link text-white" href="#">>> Cursos</a>
+            <a class="nav-link text-white" href="../admin/cursos.php">>> Cursos</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link text-white" href="#">>> Usuarios</a>
+            <a class="nav-link text-white" href="../admin/usuarios.php">>> Usuarios</a>
         </li>
     </ul>
     <div class="text-center mt-4">
---
+++ admin/usuarios.php
@@ -0,0 +1,33 @@
+<?php
+require_once 'elements/validationSession.php';
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panel Administrativo</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/dashboard.css">
+    <script src="js/inactivity.js" defer></script>
+</head>
+
+<body>
+    <?php
+    include 'elements/headeradmin.php';
+    ?>
+    <div class="container-fluid">
+        <div class="row">
+            <?php
+            include 'elements/sidebar.php';
+            ?>
+
+
+        </div>
+    </div>
+</body>
+
+</html>
