15c6b97fbbc16254daecf29faedd08e5528ee676
jafeth gamboa <jafethgamboabaas@gmail.com>
2025-05-03 03:42:26 -0600

    Refactor code structure for improved readability and maintainability

--- admin/cursos.php
+++ admin/cursos.php
@@ -12,6 +12,7 @@ require_once 'elements/validationSession.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
     <link rel="stylesheet" href="css/dashboard.css">
+    <link rel="stylesheet" href="css/cursos.css">
     <script src="js/inactivity.js" defer></script>
 </head>
 
@@ -25,6 +26,15 @@ require_once 'elements/validationSession.php';
             include 'elements/sidebar.php';
             ?>
 
+            <!-- Main Content -->
+            <main class="col-md-9 col-lg-10 py-1 border main-content">
+                <h2 class="mb-4 tittle">CURSOS</h2>
+                <hr class="custom-divider mb-4">
+                <div class="d-flex justify-content-center gap-3">
+                    <a href="ver_cursos.php" class="btn btn-dark">VER CURSOS</a>
+                    <a href="agregar_cursos.php" class="btn btn-primary custom-btn">AGREGAR CURSOS</a>
+                </div>
+            </main>
         </div>
     </div>
 </body>
