66ad0473321818ae2dee1251cf5f3d00141c273a
jafeth gamboa <jafethgamboabaas@gmail.com>
2025-05-05 15:24:08 -0600

    Agregar cláusula IF NOT EXISTS a la creación de la base de datos y tablas

--- admin/db/db_cursos.sql
+++ admin/db/db_cursos.sql
@@ -1,11 +1,11 @@
 -- Crear la base de datos
-CREATE DATABASE db_curso;
+CREATE DATABASE IF NOT EXISTS db_curso;
 
 -- Usar la base de datos
 USE db_curso;
 
 -- Estructura de tabla para la tabla `usuarios`
-CREATE TABLE `usuarios` (
+CREATE TABLE IF NOT EXISTS `usuarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `usuario` varchar(50) NOT NULL UNIQUE,
   `password` varchar(255) NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Estructura de tabla para la tabla `cursos`
-CREATE TABLE `cursos` (
+CREATE TABLE IF NOT EXISTS `cursos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) NOT NULL,
   `docente` varchar(100) NOT NULL,
@@ -24,5 +24,18 @@ CREATE TABLE `cursos` (
   `dias` varchar(100) NOT NULL,
   `objetivo` text NOT NULL,
   `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Estructura de tabla para la tabla `inscripciones`
+CREATE TABLE IF NOT EXISTS `inscripciones` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `curso` varchar(100) NOT NULL,
+  `pago` enum('Tarjeta', 'Transferencia', 'Efectivo') NOT NULL,
+  `comentarios` text DEFAULT NULL,
+  `fecha_inscripcion` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
